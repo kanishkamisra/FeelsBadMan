43f2ea2743de5eef7868997dd9b133aa58c58665
vincentvangoat <menogetusername@gmail.com>
2015-11-03 00:18:39 -0500

    Added replacement functionality

--- feelsbadman.js
+++ feelsbadman.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$('body').html(function(index, text) {
-		this.innerHTML = text.replace("FeelsBadMan"g, "<img src='replace.png'/>")
-	})
+	$('body').each(function() {
+		string = $(this).text('FeelsBadMan');
+		string.replaceWith("<img src='replace.png'/>");
+	});
 });
